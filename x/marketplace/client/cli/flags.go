02668a9afca35af1d74f665b4def50d859882efa
harish <harishmarri551@gmail.com>
2021-10-20 20:42:56 +0530

    update: marketplace query and transaction commands

--- x/marketplace/client/cli/flags.go
+++ x/marketplace/client/cli/flags.go
@@ -1,1 +1,27 @@
 package cli
+
+import (
+	flag "github.com/spf13/pflag"
+)
+
+const (
+	FlagDenomId = "denom-id"
+	FlagNftId   = "nft-id"
+	FlagPrice   = "price"
+)
+
+var (
+	FsListNft     = flag.NewFlagSet("", flag.ContinueOnError)
+	FsEditListing = flag.NewFlagSet("", flag.ContinueOnError)
+	FsBuyNFT      = flag.NewFlagSet("", flag.ContinueOnError)
+)
+
+func init() {
+	FsListNft.String(FlagDenomId, "", "nft denom id")
+	FsListNft.String(FlagNftId, "", "nft id")
+	FsListNft.String(FlagPrice, "", "listing price of nft")
+
+	FsEditListing.String(FlagPrice, "", "listing price of nft")
+
+	FsBuyNFT.String(FlagPrice, "", "buying price of nft")
+}
